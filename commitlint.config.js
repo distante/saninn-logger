bd2e09110ae078749c4d5232f51dc26eafbf1b69
distante <5490201+distante@users.noreply.github.com>
2019-01-23 18:14:17 +0100

    fix: commitlint accepts release with capital r

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const types = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'Release'];
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  'type-enum': [2, 'always', types]
+};
