824bcb11b10e5494f2fd8cbc5b1ee2fb9b12b62a
Saninn <5490201+distante@users.noreply.github.com>
2019-01-23 17:45:09 +0100

    chore: prepare scripts for publish to npm
    * feat: npm-bump and scripts
    
    * fix: removed private key from package.json
    
    * feat: release-it install
    
    * chore: release-it initial config
    
    * feat: zip scripts (requires global bestzip)
    
    * feat: release-it scripts
    
    * fix: build script with bestzip dependency

---
+++ scripts/release_3_after-bump.js
@@ -0,0 +1,30 @@
+// tslint:disable:no-string-literal
+// tslint:disable:no-console
+const scriptName = 'RELEASE IT AFTER BUMP';
+const shell = require('shelljs');
+const path = require('path');
+const distBundlesDir = path.join(process.cwd(), 'dist-bundles');
+
+const version = process.argv[2] && process.argv[2].length ? process.argv[2] + '-' : '';
+
+console.log(`\n🚀🚀🚀    ${scriptName}    🚀🚀🚀 \n`);
+if (version === '') {
+  console.log('There was no version given, zip files were not renamed');
+} else {
+  const bundleFiles = shell.find(`${distBundlesDir}/*.zip`);
+
+  bundleFiles.forEach(zipFileCompletePath => {
+    const file = path.parse(zipFileCompletePath);
+
+    if (file.name.indexOf(version) !== -1) {
+      console.warn(`❗ ${file.name}${file.ext} already contains the version`);
+      return;
+    }
+
+    const newName = path.join(file.dir, `${version}${file.name}${file.ext}`);
+    shell.mv(zipFileCompletePath, newName);
+    console.log('📄  -> 📄  zip file renamed: ' + newName);
+  });
+}
+
+console.log(`\n👍  ${scriptName} done 👍 `);
