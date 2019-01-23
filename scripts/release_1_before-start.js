a4528fd6d79abeec5f08ee72b1900babd4f96d87
distante <5490201+distante@users.noreply.github.com>
2019-01-23 15:36:57 +0100

    feat: release-it scripts

---
+++ scripts/release_1_before-start.js
@@ -0,0 +1,42 @@
+// tslint:disable:no-string-literal
+// tslint:disable:no-console
+const scriptName = 'RELEASE IT BEFORE START';
+const shell = require('shelljs');
+const path = require('path');
+const objectWithRawScripts = require(path.join(process.cwd(), 'package.json')).scripts;
+
+const packageScripts = {
+  build: objectWithRawScripts['build'],
+  buildBundles: objectWithRawScripts['build-bundles'],
+  prettierCheck: objectWithRawScripts['prettier:check'],
+  test: objectWithRawScripts['test'],
+  tscCheck: objectWithRawScripts['tsc:check']
+};
+
+function runScript(scriptToRun) {
+  try {
+    console.log(`\n
+🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  🏃‍  🏃‍  🏃‍  🏃‍  🏃‍  🏃‍  
+🏃‍  
+🏃‍    ☕   Running ${scriptToRun} 
+🏃‍  
+🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  ‍🏃‍  🏃‍  🏃‍  🏃‍  🏃‍  🏃‍  🏃‍  \n`);
+    shell.exec(scriptToRun);
+  } catch (e) {
+    shell.echo('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    shell.echo(`there was an error with ${scriptToRun}`);
+    console.error(e);
+    shell.echo('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    return false;
+  }
+  return true;
+}
+
+console.log(`\n🚀🚀🚀    ${scriptName}    🚀🚀🚀 \n`);
+
+runScript(packageScripts.test + ' --silent'); // npm run test
+runScript(packageScripts.tscCheck); // npm run tsc:check
+runScript(packageScripts.build); // npm run build
+runScript(packageScripts.buildBundles); // npm run build-bundles",  //this also zips
+
+console.log(`\n👍  ${scriptName} done 👍 `);
