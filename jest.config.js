5239aa2d086e8fff6613781d7d1d3e5a9208ab24
distante <5490201+distante@users.noreply.github.com>
2019-03-09 12:38:02 +0100

    chore: update jest and rollup

--- jest.config.js
+++ jest.config.js
@@ -127,7 +127,7 @@ module.exports = {
   // setupFiles: [],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  setupTestFrameworkScriptFile: 'jest-extended',
+  setupFilesAfterEnv: ['jest-extended'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
