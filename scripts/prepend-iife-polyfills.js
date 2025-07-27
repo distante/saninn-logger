f7386c11d90f67501d2f2e6261fe5c6408298133
Saninn Salas Diaz <5490201+distante@users.noreply.github.com>
2025-07-27 09:16:40 +0200

    chore: removed object.keys dev dependency

--- scripts/prepend-iife-polyfills.js
+++ scripts/prepend-iife-polyfills.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const polyfillPath = path.join(process.cwd(), 'node_modules', 'proxy-polyfill', 'proxy.min.js');
-// const objectKeysPath = path.join(process.cwd(), 'node_modules', 'object.keys', 'index.js');
+
 const bundlePath = path.join(process.cwd(), 'dist-bundles', 'iife', '@saninn__logger.js');
 
 const files = [polyfillPath, bundlePath];
 
 var output = files
-  .map(f => {
+  .map((f) => {
     return fs.readFileSync(f).toString();
   })
   .join(';');
